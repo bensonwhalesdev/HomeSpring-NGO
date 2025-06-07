514fd4786e4ee5b86e02537abc5ab11088b9e8e1
bensonwhalesdev <lawrencebenson18@hotmail.com>
2025-06-07 04:56:27 -0700

    file updated

--- src/components/Herosection/index.jsx
+++ src/components/Herosection/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../Button';
+import 'animate.css'
 
 const HeroSection = () => {
   return (
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <img className="w-full h-full object-cover" src="/borehole.webp" alt="hero-img"/>
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
 
-      <div className="absolute top-1/2 left-6 md:left-12 lg:left-20 transform -translate-y-1/2 text-white z-10 w-[90%] md:w-[70%]">
+      <div className="absolute top-1/2 left-6 md:left-12 lg:left-20 transform -translate-y-1/2 text-white z-10 w-[90%] md:w-[70%] animate__animated animate__bounceInDown">
         <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold leading-tight">Clean water, <br /> brighter futures
         </h1>
         <p className="mt-3 text-sm md:text-lg lg:text-xl">Our objective is to bring clean and sustainable water to 300 villages by end of 2026
